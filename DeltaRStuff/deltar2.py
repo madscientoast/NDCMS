05e9a489f5cf9b074908d53229c93025584ce164
Robert Snuggs <rsnuggs@nd.edu>
2024-01-04 13:00:52 -0500

    Updated DeltaR

--- DeltaRStuff/deltar2.py
+++ DeltaRStuff/deltar2.py
@@ -1,7 +1,9 @@
 from ROOT import TH1D, TFile, TLorentzVector, TCanvas
+from itertools import combinations
+from more_itertools import unique_everseen
 import time
 
-def FindAndSetMax(h): 
+def FindAndSetMax(h): #Marc's code
 	maximum = 0.0
 	h.SetStats(0)
 	t = h.GetMaximum()
@@ -11,29 +13,28 @@ def FindAndSetMax(h):
 	h.SetLineWidth(2)
 	return maximum*1.35
 
-
-c = TCanvas()
+canv = TCanvas()
 h = TH1D("h1", "dR [X300A15]", 2000, 0.0, 100.0)
-f = TFile.Open("X3000A750/3A3C76AE-9E9D-5545-8A4C-B4E443FE2F36.root")
+f = TFile.Open("X300A15/8FC216FE-6399-6845-8572-36A5078139F1.root")
 events = f.Events
 
 start = time.time()
+
 for e in range(0,events.GetEntries()):
-    events.GetEntry(e) 
+    events.GetEntry(e)
     pt = events.Photon_pt
     eta = events.Photon_eta
     phi = events.Photon_phi 
     m = events.Photon_mass
-    for i in range(0,len(pt)):
+    vec = [[pt[i],eta[i],phi[i],m[i]] for i in range(len(pt))]
+    comb = unique_everseen(combinations(vec,2))
+    for c in comb:
         photon0 = TLorentzVector()
-        photon0.SetPtEtaPhiM(pt[i],eta[i],phi[i],m[i])
-        for j in range(0,len(pt)):
-            if i >= j:
-                continue
-            photon1 = TLorentzVector()
-            photon1.SetPtEtaPhiM(pt[j],eta[j],phi[j],m[j])
-            dR = photon1.DeltaR(photon0)
-            h.Fill(dR)
+        photon1 = TLorentzVector()
+        photon0.SetPtEtaPhiM(c[0][0],c[0][1],c[0][2],c[0][3])
+        photon1.SetPtEtaPhiM(c[1][0],c[1][1],c[1][2],c[1][3])
+        dR = photon1.DeltaR(photon0)
+        h.Fill(dR)
 
 print("Time Elapsed: ", time.time()-start) 
 
@@ -41,4 +42,4 @@ x = h.GetXaxis()
 FindAndSetMax(h)
 x.SetRangeUser(0, 10)
 h.Draw()
-c.SaveAs("X300A15/test.pdf")
+canv.SaveAs("X300A15/test2.pdf")
